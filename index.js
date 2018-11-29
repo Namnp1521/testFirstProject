977e1b8a0edca2067074eb06ff3b945dc12202a5
Namnp1521 <45452919+Namnp1521@users.noreply.github.com>
2018-11-29 17:08:48 +0700

    abc
    abc

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿var express = require("express");
+var app = express();
+
+var loginFilter = require('./filters/loginFilter');
+loginFilter.loginFilter(app);
+
+var userRouter = require('./routers/router');
+userRouter.router(app);
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.listen(3000);
